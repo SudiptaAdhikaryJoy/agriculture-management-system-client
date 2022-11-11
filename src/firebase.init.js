28e11f5f0ece43f92d9132a44f00ea75f7ca534c
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-11-12 00:03:31 +0600

    firebase setting up

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBcrQdneQ8oS8xs4bIF6ZHmMZzKAuTwINg",
+	authDomain: "agri-app-4c043.firebaseapp.com",
+	projectId: "agri-app-4c043",
+	storageBucket: "agri-app-4c043.appspot.com",
+	messagingSenderId: "804227574711",
+	appId: "1:804227574711:web:91da8bcfe53f30f08a9632",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
